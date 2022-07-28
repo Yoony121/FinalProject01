02ea05269798e35fd10968c2f173b1071581e1db
Yoony121 <110114135+Yoony121@users.noreply.github.com>
2022-07-28 18:56:55 +0200

    Update RefundPolicy.jsx

--- react_frontend_client/src/components/RefundPolicy.jsx
+++ react_frontend_client/src/components/RefundPolicy.jsx
@@ -7,7 +7,7 @@ const RefundPolicy = () => {
             <main>
                 <div className="refundPolicy">
                     <h2 className="heading">Refund Policy</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga voluptates aperiam inventore illo unde non, fugit, a ipsum, voluptas nostrum eum beatae quis soluta natus saepe consequuntur quos magnam animi?</p>
+                    <p>Email: customerservice@gmail.com</p>
                 </div>
             </main>
         </div>
